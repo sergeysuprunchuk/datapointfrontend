bc9f81f7acaa860d9df8d7d1a3e17e041987e947
sergeysuprunchuk <sergeysuprunchuk@yandex.ru>
2024-06-27 17:02:15 +0700

    добавлена темная тема

--- src/components/layouts/MainLayout.vue
+++ src/components/layouts/MainLayout.vue
@@ -1,13 +1,15 @@
 <script lang="ts" setup>
 import HeaderMenu from "../headerMenu/HeaderMenu.vue"
+import ThemeSwitch from "../themeSwitch/ThemeSwitch.vue"
 </script>
 
 <template>
 	<div class="h-screen w-screen overflow-hidden">
 		<header
-			class="flex justify-center border-b border-surface-300 py-4 px-6 bg-surface-0"
+			class="flex items-center justify-between border-b primary-border py-4 px-6 primary-background"
 		>
 			<header-menu />
+			<theme-switch />
 		</header>
 		<div class="h-full w-full overflow-auto">
 			<slot />
---
+++ src/components/themeSwitch/ThemeSwitch.vue
@@ -0,0 +1,50 @@
+<script lang="ts" setup>
+import { AppButton } from "../../ui"
+import { computed, onMounted, ref } from "vue"
+
+const darkMode = ref<boolean>(false)
+
+const icon = computed<string>(() => {
+	return darkMode.value ? "pi pi-moon" : "pi pi-sun"
+})
+
+const DARK_CLASS: string = "dark"
+const LIGHT_CLASS: string = "light"
+const KEY = "theme"
+
+const onClick = () => {
+	if (darkMode.value) {
+		document.documentElement.classList.remove(DARK_CLASS)
+		localStorage.setItem(KEY, LIGHT_CLASS)
+	} else {
+		document.documentElement.classList.add(DARK_CLASS)
+		localStorage.setItem(KEY, DARK_CLASS)
+	}
+	darkMode.value = !darkMode.value
+}
+
+onMounted(() => {
+	const theme = localStorage.getItem(KEY)
+	switch (theme) {
+		case LIGHT_CLASS:
+			return
+
+		case DARK_CLASS:
+			darkMode.value = true
+			document.documentElement.classList.add(DARK_CLASS)
+			return
+
+		default:
+			localStorage.setItem(KEY, LIGHT_CLASS)
+			return
+	}
+})
+</script>
+
+<template>
+	<app-button
+		:icon="icon"
+		@click="onClick"
+		text
+	/>
+</template>
